699eb35ffe9c1c47a30fe29c04ae7355290815c5
Guilherme Lima <guilherme.lima.dev@protonmail.com>
2022-10-10 19:21:26 +0100

    mv challenge_1 to challenge1

---
+++ challenges/set1/challenge1/src/main.rs
@@ -0,0 +1,14 @@
+use encoding::{Encode, Decode};
+
+static HEX_INPUT: &str = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+static EXPECTED_B64_OUTPUT: &str = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+
+fn main() {
+    let decoded_input = Vec::from_hex(HEX_INPUT).unwrap();
+    let b64_out = decoded_input.to_base64();
+    
+    println!("Hexadecimal input: {}", HEX_INPUT);
+    println!("Base64 output: {}", b64_out);
+
+    assert_eq!(b64_out, EXPECTED_B64_OUTPUT);
+}

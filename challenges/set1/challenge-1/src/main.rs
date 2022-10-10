9890d9a634bed0d1bd2992c7295fc24cbdc1c387
Guilherme Lima <guilherme.lima.dev@protonmail.com>
2022-10-10 15:16:11 +0100

    remove 'static from static string declarations on challenge-1

--- challenges/set1/challenge-1/src/main.rs
+++ challenges/set1/challenge-1/src/main.rs
@@ -1,7 +1,7 @@
 use encoding::{Encode, Decode};
 
-static HEX_INPUT: &'static str = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-static EXPECTED_B64_OUTPUT: &'static str = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+static HEX_INPUT: &str = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+static EXPECTED_B64_OUTPUT: &str = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
 
 fn main() {
     let decoded_input = Vec::from_hex(HEX_INPUT).unwrap();

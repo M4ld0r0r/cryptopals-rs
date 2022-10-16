3528f493cd3394d823c998eafe72b3d9c3bcf6a5
Guilherme Lima <guilherme.lima.dev@protonmail.com>
2022-10-16 19:37:51 +0100

    finished challenge 3

--- challenges/set1/challenge3/src/main.rs
+++ challenges/set1/challenge3/src/main.rs
@@ -1,3 +1,45 @@
+// Single-byte XOR cipher
+//
+// The hex encoded string:
+//
+// 1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736
+//
+// ... has been XOR'd against a single character. Find the key, decrypt the message.
+//
+// You can do this by hand. But don't: write code to do it for you.
+//
+// How? Devise some method for "scoring" a piece of English plaintext.
+// Character frequency is a good metric. Evaluate each output and choose the one with the best score.
+
+use std::str;
+
+use cryptanalysis::calculate_lang_score;
+use encoding::Decode;
+use xor::xor;
+
+static CIPHERTEXT: &str = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+
 fn main() {
-    println!("Hello, world!");
+    let ciphertext_bytes = Vec::from_hex(CIPHERTEXT).unwrap();
+
+    let mut best_key = 0x00;
+    let mut best_key_score = 0;
+
+    // brute force attack + frequency analysis
+    for key in 0x00..=0xFF {
+        let xor_result = xor(&ciphertext_bytes, &[key]);
+        let key_score = calculate_lang_score(&xor_result, "EN");
+
+        if key_score > best_key_score {
+            best_key = key;
+            best_key_score = key_score;
+        }
+    }
+
+    let plaintext_bytes = xor(&ciphertext_bytes, &[best_key]);
+    let plaintext = str::from_utf8(&plaintext_bytes).unwrap();
+
+    println!("Key: {}", best_key);
+    println!("Plaintext: {}", plaintext);
+    println!("Score: {}/12", best_key_score);
 }
--- crates/cryptanalysis/src/lib.rs
+++ crates/cryptanalysis/src/lib.rs
@@ -1,14 +1,37 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+mod utils;
+use utils::*;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+/// Calculate how likely it is for the given text to be written in the given language
+///
+/// # Args
+/// `text`: The text we want to analyze
+///
+/// `lang`: The source language abbrevation (ex: English -> "EN")
+///
+/// # Returns
+/// A `usize` value between 0 and 12, 0 meaning that the text is definitely not written in
+/// the language and 12 meaning that the language is definitely written in the source language
+pub fn calculate_lang_score(text: &[u8], lang: &str) -> usize {
+    let source_lang_freqs = get_source_lang_freqs(lang).as_bytes();
+    let mut score = 0;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    let letter_counts = get_letter_counts(text);
+    
+    // sort letter counts from highest to lowest
+    let mut c: Vec<_> = letter_counts.iter().collect();
+    c.sort_by(|a, b| b.1.cmp(a.1));
+
+    // check if the 6 most and least frequent chars match those of the source language
+    for (u, _) in &c[0..6] {
+        if source_lang_freqs[0..6].contains(*u) {
+            score += 1;
+        }
     }
+    for (u, _) in &c[19..26] {
+        if source_lang_freqs[19..26].contains(*u) {
+            score += 1;
+        }
+    }
+
+    score
 }

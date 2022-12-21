25d8a2a0bc760d7362332e356a1532863b6f0e82
Guilherme Lima <guilherme.lima.dev@protonmail.com>
2022-12-21 23:31:03 +0000

    finished challenge09

---
+++ challenges/set02/challenge09/src/main.rs
@@ -0,0 +1,16 @@
+use primitives::pad_pkcs7;
+
+static BLOCK_SIZE: usize = 20;
+
+static TEXT: &str = "YELLOW SUBMARINE";
+static EXPECTED: &str = "YELLOW SUBMARINE\u{4}\u{4}\u{4}\u{4}";
+
+fn main() {
+    let result = pad_pkcs7(TEXT.as_bytes(), BLOCK_SIZE).unwrap();
+    let result = std::str::from_utf8(&result).unwrap();
+   
+    println!("Input text: {}", TEXT);
+    println!("Padded to 20 bytes block size: {:?}", result);
+    
+    assert_eq!(result, EXPECTED);
+}

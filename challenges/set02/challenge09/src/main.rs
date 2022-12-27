74b0b4efb277ce367498a6654c5eff37349e057b
Guilherme Lima <guilherme.lima.dev@protonmail.com>
2022-12-27 00:27:21 +0000

    cargo fmt

--- challenges/set02/challenge09/src/main.rs
+++ challenges/set02/challenge09/src/main.rs
@@ -8,9 +8,9 @@ static EXPECTED: &str = "YELLOW SUBMARINE\u{4}\u{4}\u{4}\u{4}";
 fn main() {
     let result = pad_pkcs7(TEXT.as_bytes(), BLOCK_SIZE).unwrap();
     let result = std::str::from_utf8(&result).unwrap();
-   
+
     println!("Input text: {}", TEXT);
     println!("Padded to 20 bytes block size: {:?}", result);
-    
+
     assert_eq!(result, EXPECTED);
 }
